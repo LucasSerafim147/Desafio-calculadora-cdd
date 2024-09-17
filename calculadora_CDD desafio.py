e5906ee4d340553fe80e279c00bd0158ba5db889
Lucas Henrique Serafim da Silva <165844637+LucasSerafim147@users.noreply.github.com>
2024-09-17 19:18:44 -0300

    Add files via upload

---
+++ calculadora_CDD desafio.py
@@ -0,0 +1,32 @@
+
+while True:
+
+    n1 = float(input("Insira o primeiro numero: "))
+    n2 = float(input("Insira o segundo numero: "))
+
+    op = int(input("1-Adição\n2-Subtração\n3-Divisão\n4-Multiplicação\n5-Tentar outro numero\n6-Sair\nInsira:"))
+
+    if op == 1:
+        resultado = n1 + n2
+        print(resultado)
+        break
+    elif op == 2 :
+        resultado = n1 - n2
+        print(resultado)
+        break
+    elif op == 3 :
+        resultado = n1 / n2
+        print(resultado)
+        break
+    elif op == 4:
+        resultado = n1 + n2
+        print(resultado)
+        break
+    elif op == 5:
+        
+        continue
+    elif op ==6:
+        break
+       
+
+        
